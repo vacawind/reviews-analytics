589e0cce9faf4dd0e6ac2d17e2e9850e1605e30e
JJ Lee <vacawind@gmail.com>
2020-10-11 11:12:39 +0800

    update version

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -2,12 +2,18 @@
 
 data = []
 count = 0
+string = 0
 with open('reviews.txt','r') as f:
 	for line in f: #每次讀取一行，故取名為line
 		data.append(line)
+		string = string + len(line)
 		count = count + 1
 		if count % 1000 == 0:
 			print(count) #每讀1000行後印讀取進度
 
+average_len = string / len(data) #每筆留言平均長度
+
 print("檔案讀取結束，共有",len(data),"筆資料。")
+
+print("每筆留言平均長度為",average_len)
 
